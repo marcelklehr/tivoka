cd00c3f7648e2b096ceb25b16833c6830181a717
Marcel Klehr <marcel.klehr@gmx.de>
2012-03-07 17:00:57 +0100

    Corrected include

--- include.php
+++ include.php
@@ -2,15 +2,15 @@
 include dirname(__FILE__).'/Tivoka.php';
 include dirname(__FILE__).'/Exception.php';
 
-include dirname(__FILE__).'/Connection.php';
-include dirname(__FILE__).'/Request.php';
-include dirname(__FILE__).'/Notification.php';
-include dirname(__FILE__).'/BatchRequest.php';
+include dirname(__FILE__).'/client/Connection.php';
+include dirname(__FILE__).'/client/Request.php';
+include dirname(__FILE__).'/client/Notification.php';
+include dirname(__FILE__).'/client/BatchRequest.php';
 
-include dirname(__FILE__).'/Response.php';
-include dirname(__FILE__).'/BatchResponse.php';
+include dirname(__FILE__).'/client/Response.php';
+include dirname(__FILE__).'/client/BatchResponse.php';
 
-include dirname(__FILE__).'/Server.php';
-include dirname(__FILE__).'/Processor.php';
-include dirname(__FILE__).'/MethodWrapper.php';
+include dirname(__FILE__).'/server/Server.php';
+include dirname(__FILE__).'/server/Processor.php';
+include dirname(__FILE__).'/server/MethodWrapper.php';
 ?>
