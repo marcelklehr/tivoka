b8b57ad22cbdb26b120c3d378b1184c43a195b09
Marcel Klehr <marcel.klehr@gmx.de>
2012-03-11 14:30:52 +0100

    Simplified server example

--- example/server.php
+++ example/server.php
@@ -4,20 +4,13 @@ include('../include.php');
 $methods = array(
 	'demo.sayHello' => function($request)
 	{
-		$request->returnResult('Hello World!');
+		$request->result('Hello World!');
 	},
 	
 	'demo.substract' => function($request)
 	{
-		if(!is_array($request->params)) return $request->returnError(-32602);
-		$tmp = array_keys($request->params);
-		if(	!count($request->params) == 2 || 
-			!is_numeric($request->params[array_pop($tmp)]) ||
-			!is_numeric($request->params[array_pop($tmp)]) )
-		{
-			return $request->returnError(-32602);
-		}
-		return $request->returnResult(intval($request->params[0]) - intval($request->params[1]));
+		if(!is_array($request->params)) return $request->error(-32602);
+		return $request->result(intval($request->params[0]) - intval($request->params[1]));
 	}
 );
 
