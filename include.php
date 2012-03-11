b6515b2976bdcbd9f205f684ec933a0be160239d
Marcel Klehr <marcel.klehr@gmx.de>
2012-03-11 15:25:06 +0100

    unincluded Response files

--- include.php
+++ include.php
@@ -8,9 +8,6 @@ include dirname(__FILE__).'/client/Notification.php';
 include dirname(__FILE__).'/client/BatchRequest.php';
 include dirname(__FILE__).'/client/Client.php';
 
-include dirname(__FILE__).'/client/Response.php';
-include dirname(__FILE__).'/client/BatchResponse.php';
-
 include dirname(__FILE__).'/server/Server.php';
 include dirname(__FILE__).'/server/Processor.php';
 include dirname(__FILE__).'/server/MethodWrapper.php';
